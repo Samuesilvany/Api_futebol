97d74cda72116652956f21ff05050f972262318f
Samuel <samuel_silvany@estudante.sesisenai.org.br>
2026-04-29 17:12:40 -0300

    29-04-26

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import fs from "fs/promises";
 
 
+import  app  from './src/config/times.json';
+import { assert } from "console";
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
